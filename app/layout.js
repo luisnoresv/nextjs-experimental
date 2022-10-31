c867022d99e6ea9e03f359c620d3a8b65a8907b3
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2022-10-31 17:03:47 -0500

    Adding comments

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,10 @@ import NavLink from './nav-link';
  */
 
 /**
- * * Next 13 Server components: Run on the server and send their static output up to the browser +Fast,-less js to the FE
- * ! Next 12: Pre-rendering client components, sending the HTML but also the js to rehydrate those components on the client
+ * * 😏 Next 13 Server components: Run on the server and send their static
+ * * output up to the browser +Fast,-less js to the FE
+ * * 🧐 Next 12: Pre-rendering client components, sending the HTML but also the js to rehydrate
+ * * those components on the client
  */
 export default function RooLayout({ children }) {
 	console.log('rendering');
--- app/nav-link.js
+++ app/nav-link.js
@@ -6,6 +6,7 @@ import { useSelectedLayoutSegments } from 'next/navigation';
  * * Nextjs 13: we can create components anyware
  */
 export default function NavLink({ href, children }) {
+	// Get the canonical segment path from the current level to the leaf node.
 	let segment = useSelectedLayoutSegments();
 	let active = href === `/${segment}`;
 
